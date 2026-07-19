d5dfac6ffc8f1b2968a849f8eb6aa5cdc3ff275f
EC2 Default User <ec2-user@ip-172-31-30-217.ap-south-2.compute.internal>
2026-07-19 21:32:25 +0000

    Mock database connection for CI tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,5 +1,6 @@
 import sys
 import os
+from unittest.mock import patch, MagicMock
 
 sys.path.insert(
     0,
@@ -14,14 +15,35 @@ from app import app
 
 @pytest.fixture
 def client():
+
     app.config["TESTING"] = True
 
     with app.test_client() as client:
         yield client
 
 
-def test_home_page(client):
+
+@patch("app.get_connection")
+def test_home_page(mock_connection, client):
+
+    # Fake database connection
+    mock_conn = MagicMock()
+
+    # Fake database cursor
+    mock_cursor = MagicMock()
+
+    # Fake database response
+    mock_cursor.fetchall.return_value = [
+        (1, "John", 5000)
+    ]
+
+
+    mock_conn.cursor.return_value = mock_cursor
+
+    mock_connection.return_value = mock_conn
+
 
     response = client.get("/")
+
 
     assert response.status_code == 200
